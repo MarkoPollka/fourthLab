a958bcf8880cf0751e3c4624af2d90d397dd5d04
Vlad Savchenko <polkamaraka@gmail.com>
2020-04-30 13:13:55 +0300

    Initial commit

---
+++ app/src/main/java/it_school/sumdu/edu/fourthlab/MainActivity.java
@@ -0,0 +1,29 @@
+package it_school.sumdu.edu.fourthlab;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.txtView);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
